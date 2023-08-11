3d6f4e5da7a5f3885dc0a3470666436be145f95f
samuel212121 <abemmenelik@gmail.com>
2023-08-10 17:02:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A programe that print standardized error
- * Return: 1 (Success)
+ * main - A programe that print a line to the    standardized error
+ * Return 1 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
