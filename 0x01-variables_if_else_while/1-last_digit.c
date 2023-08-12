0de8e697fbc166e8e1e56cc04d76cab35b76b11b
samuel212121 <abemmenelik@gmail.com>
2023-08-11 19:15:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		prrintf("%d and is less than 6 doesnot 0\n", n, n % 10);
+		printf("%d and is less than 6 doesnot 0\n", n, n % 10);
 	}
 	else
 	{
