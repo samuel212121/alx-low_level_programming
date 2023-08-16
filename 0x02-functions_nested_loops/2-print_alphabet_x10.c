52c21efdb0be15751dc3f9334dafe5632935c659
samuel212121 <abemmenelik@gmail.com>
2023-08-15 21:30:54 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main .h"
+/**
+ * print_alphabet_x10 - print 10 times alpha in lower case
+ * followed by new line 
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int x;
+
+x = 0;
+while (x < 0)
+{
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++
+	}
+	_putchar('\n');
+	x++
+}
+			
+
+	
+
+		
+
+
