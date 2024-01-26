e441ba955addb9d3a1741eaaff0f183f48ae4a78
samuel212121 <abemmenelik@gmail.com>
2024-01-26 07:31:32 -0800

    add 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,12 +13,14 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
 			l++;
 	}
 	l += ac;
+
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
@@ -36,3 +38,4 @@ char *argstostr(int ac, char **av)
 	}
 	return (str);
 }
+
