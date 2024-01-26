ccde5bf771d2f0ba4bf409a5fd9d5b2c061ddb61
samuel212121 <abemmenelik@gmail.com>
2024-01-25 23:52:09 -0800

    add main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char);
+
+_putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,5 +12,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif
