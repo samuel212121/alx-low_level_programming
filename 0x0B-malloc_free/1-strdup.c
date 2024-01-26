e559b539acff5108512ac0f7b34dd56d08d806ce
samuel212121 <abemmenelik@gmail.com>
2024-01-26 07:12:44 -0800

    add 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,10 +16,15 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
+
 	aaa = malloc(sizeof(char) * (i + 1));
+
 	if (aaa == NULL)
 		return (NULL);
+
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
+
 	return (aaa);
 }
+
